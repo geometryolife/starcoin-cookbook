d57a94a1e11005af50b6f4ba6e1b28d175f869d7
Joe Chen <geometryolife@gmail.com>
2022-04-22 20:01:35 +0800

    add some comments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,24 +1,26 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer/themes/github'); // 导入明亮主题
+const darkCodeTheme = require('prism-react-renderer/themes/dracula'); // 导入暗色主题
 
+// 下面这行应该是装饰器？
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Starcoin Cookbook',
-  tagline: 'How to developing on starcoin',
-  url: 'https://starcoinorg.github.io',
-  baseUrl: '/starcoin-cookbook/',
+  title: 'Starcoin Cookbook', // 书的标题
+  tagline: 'How to developing on starcoin', // 类似副标题
+  url: 'https://starcoinorg.github.io', // 统一资源定位（前缀）
+  baseUrl: '/starcoin-cookbook/', // 和 url 共同组成本书的资源定位
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: '/img/favicon.ico',
+  favicon: '/img/favicon.ico', // 网站标志，p114
   organizationName: 'staroinorg', // Usually your GitHub org/user name.
   projectName: 'starcoin-cookbook', // Usually your repo name.
   
+  // i18n国际化
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'zh'],
+    defaultLocale: 'en', // 设置默认的 locale 值
+    locales: ['en', 'zh'], // locale 的语言列表
     localeConfigs: {
       en: {
         label: 'English',
@@ -30,59 +32,62 @@ const config = {
       },
     },
   },
+
+  // 预设
   presets: [
     [
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve('./sidebars.js'), // 侧边连模块
           // Please change this to your repo.
-          editUrl: 'https://github.com/starcoinorg/starcoin-cookbook/edit/main/',
+          editUrl: 'https://github.com/starcoinorg/starcoin-cookbook/edit/main/', // 编辑此页按钮
         },
         blog: {
-          showReadingTime: true,
+          showReadingTime: true, // ???
           // Please change this to your repo.
           editUrl:
             'https://github.com/starcoinorg/starcoin-cookbook/edit/main/',
         },
-        theme: {
+        theme: { // 通过 CSS 自定义主题
           customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
   ],
 
+  // 主题设置
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      navbar: {
+      navbar: { // 导航栏配置
         title: 'Starcoin Cookbook',
         logo: {
           alt: 'Starcoin Cookbook',
           src: '/img/logo.svg',
         },
         items: [
-          {
+          { // 点击 Document 跳转到文档页
             type: 'doc',
             docId: 'intro',
             position: 'left',
             label: 'Documents',
           },
-          {
+          { // 下拉框位置
             type: 'localeDropdown',
             position: 'right',
           },
-          {
+          { // 右上角的跳转地址 -> GitHub 仓库
             href: 'https://github.com/starcoinorg/starcoin-cookbook',
             label: 'GitHub',
             position: 'right',
           },
         ],
       },
-      footer: {
-        style: 'dark',
-        links: [
+      footer: { // 页脚配置
+        style: 'dark', // 主题风格
+        links: [ // 三列脚注链接列表
           {
             title: 'Docs',
             items: [
@@ -125,11 +130,12 @@ const config = {
         ],
         copyright: `Copyright © ${new Date().getFullYear()} Starcoin, Inc. Built with Docusaurus.`,
       },
-      prism: {
+      prism: { // ???
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
     }),
 };
 
+// 模块导出
 module.exports = config;
